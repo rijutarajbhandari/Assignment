5c1c953e9573692b3a624aefa5d47f50d8a266f8
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-22 19:23:22 +0545

    Adding haeder and footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p> $copy;  2025 QuickPay. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<! DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Responsive Home Page </title>
+    <link rel ="stylesheet" href="C:/Users/user/EBPearls/Assignment/css/style.css">
+    <script defer src="C:/Users/user/EBPearls/Assignment/js/script.js></script>
+</head>
+<body>
+
+<header>
+    <div class="logo">QuickPay</div>
+    <nav>
+        <ul>
+            <li> <a href="#"> Home </a></li>
+            <li> <a href="#"> Services </a></li>
+            <li> <a href="#"> Contact</a> </li>
+        </ul>
+    </nav>
+</header>
