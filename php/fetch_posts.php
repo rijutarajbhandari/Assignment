98e5cc1c14b8abb309ced462488cc5360657a146
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-22 18:50:03 +0545

    Retrieval of posts from database

---
+++ php/fetch_posts.php
@@ -0,0 +1,21 @@
+<?php
+include "db_Connect.php";
+
+$sql = "SELECT * FROM posts";
+$result=$conn -> query($sql);
+
+if ($result -> num_rows>0){
+    while($row->$result(fetch_assoc())){
+        echo "
+        <div class="post">
+          <img src="{$row["icon"]}" alt="Icon">
+          <h3>{$row["title"]}</h3>
+          <p>{$row["desp"]}</p>
+        </div>";
+    }
+}else{
+    echo "<p> No posts available </p>";
+}
+
+$conn -> close
+?>
