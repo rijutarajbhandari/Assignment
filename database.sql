e0620f6e0373b683bd8142ba77a3412c31b3a960
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-22 22:05:14 +0545

    Updating the database

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE posts(
 );
 
 INSERT INTO posts(icon,title,descp) VALUES
-("C:/Users/user/EBPearls/Assignment/img/secpay.jpg","Secure Payments","We provide the best security for payments"),
-("C:/Users/user/EBPearls/Assignment/img/fasttrans.jpg","Fast Transactions","All transactions are completed within seconds"),
-("C:/Users/user/EBPearls/Assignment/img/globalaccess.jpg","Global Access","Send and receive money from anywhere in the world");
+("C:/xampp/htdocs/EBPearls/Assignment/img/secpay.jpg","Secure Payments","We provide the best security for payments"),
+("C:/xampp/htdocs/EBPearls/Assignment/img/fasttrans.jpg","Fast Transactions","All transactions are completed within seconds"),
+("C:/xampp/htdocs/EBPearls/Assignment/img/globalaccess.jpg","Global Access","Send and receive money from anywhere in the world");
 
