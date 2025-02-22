6b742af59bd2a378a1755e880c64f4cdd585ef24
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-22 19:32:20 +0545

    Adding an index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php  include "includes/header.php";  ?>
+
+<section class="banner">
+    <div class="slider">
+        <img src="C:/Users/user/EBPearls/Assignment/img/secpay.jpg" alt="Slide 1">
+        <img src="C:/Users/user/EBPearls/Assignment/img/fasttrans.jpg" alt="Slide 2">
+        <img src="C:/Users/user/EBPearls/Assignment/img/globalaccess.jpg" alt="Slide 3">
+    </div>
+</section>
+
+<section class="icon-block">
+    <h2>Outsource Payment Services</h2>
+    <div id="post-container">
+        <?php include "php/fetch_posts.php"; ?>
+    </div>
+</section>
+
+<?php include "includes/footer.php"; ?>
