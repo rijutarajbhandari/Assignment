41224a63491093fa24c4b2600e93883427336ab1
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-22 18:29:45 +0545

    Updated database and addition of values

--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE assignment_db;
 USE assignment_db;
+
+CREATE TABLE posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    icon VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    desp TEXT NOT NULL
+);
+
+INSERT INTO posts(icon,title,descp) VALUES
+("C:/Users/user/EBPearls/Assignment/img/secpay.jpg","Secure Payments","We provide the best security for payments"),
+("C:/Users/user/EBPearls/Assignment/img/fasttrans.jpg","Fast Transactions","All transactions are completed within seconds"),
+("C:/Users/user/EBPearls/Assignment/img/globalaccess.jpg","Global Access","Send and receive money from anywhere in the world");
+
