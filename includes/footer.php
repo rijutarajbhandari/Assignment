65409cf1741636059a06e9232074e8cc674064d0
rijutarajbhandari <rijuta2001@gmail.com>
2025-02-23 16:05:59 +0545

    Added more elements to header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,16 @@
 <footer>
+<div class="footer-container">
+        <div class="footer-logo">
+            <span>QuickPay</span>
+        </div>
+        <div class="footer-links">
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
     <p> $copy;  2025 QuickPay. All rights reserved.</p>
 </footer>
 
--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Responsive Home Page </title>
-    <link rel ="stylesheet" href="C:/Users/user/EBPearls/Assignment/css/style.css">
-    <script defer src="C:/Users/user/EBPearls/Assignment/js/script.js"></script>
+    <link rel ="stylesheet" href="css/style.css">
+    <script defer src="js/script.js"></script>
 </head>
 <body>
 
@@ -13,9 +13,19 @@
     <div class= "logo">QuickPay</div>
     <nav>
         <ul>
-            <li> <a href="#"> Home </a></li>
-            <li> <a href="#"> Services </a></li>
-            <li> <a href="#"> Contact</a> </li>
+            <li> <a href="#"> Products </a></li>
+            <li> <a href="#"> Solutions </a></li>
+            <li> <a href="#"> Help Center</a> </li>
+            <li> <a href="#"> Request a demo</a></li>
+            <li> <a href="#"  class="login"> Login</a></li>
+            <li><button> Get a demo</button> </li>
         </ul>
     </nav>
+    <img src="img/QuickPay_logo.png">
 </header>
+
+<section class="header-image">
+    <img src="img/cashflow.jpg" alt="Description of Image" />
+    <h1> <center>Get instant cash flow with invoice factoring</center></h1>
+    <h2> Why wait? Get same day funding and a faster way to access cash flow</h2>
+</section>
